9e7c876f8f6d8021c2ca012121d3b0789b15fe0b
recholkibebe <recholkibebe@gmail.com>
2024-08-01 16:05:46 +0300

    concatente strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Write a type  function to concatenate two strings.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str:  concatenated two strings.
+    """
+    return str1 + str2
+
